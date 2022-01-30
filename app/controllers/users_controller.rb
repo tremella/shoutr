e7189802dbbc8e813e9bded81aac99b3200a9332
tremella <jeshbrand@gmail.com>
2022-01-30 14:57:29 +0000

    user page, shout page, dashboard;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < Clearance::UsersController
+    def show
+        @user = User.find(params[:id])
+        @shouts = @user.shouts
+    end
+    
     private
+
 
     def user_params
         params.require(:user).permit(:username,:email,:password)
     end
+
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -3,4 +3,9 @@ class Shout < ApplicationRecord
 
   validates :body, presence: true, length: { in: 1..144 }
   validates :user, presence: true
+
+  default_scope { order(created_at: :desc) } # to change this, we can append .reorder to this.
+  # generally avoid default_scope
+
+  delegate :username, to: :user #grabs user FROM username when we call shout.username
 end

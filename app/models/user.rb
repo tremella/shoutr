de105d2f9c5a4a840e3ad4db9d5187fe7dbad72f
tremella <jeshbrand@gmail.com>
2022-01-30 14:17:38 +0000

    user can add and see shouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true
 
+  has_many :shouts, dependent: :destroy
   # <User id: 1, 
   # created_at: "2022-01-29 04:20:69", 
   # updated_at: "2022-01-30 04:20:69", 
